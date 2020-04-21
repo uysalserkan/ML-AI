cc251dadaad0437899140a08f3eb34de5bbd5dec
Serkan UYSAL <serqkanseko@gmail.com>
2020-04-21 23:16:15 +0300

    Create Gradient_Descent3.py

---
+++ CodeAcademy Codes/Gradient_Descent3.py
@@ -0,0 +1,38 @@
+def get_gradient_at_b(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+    x_val = x[i]
+    y_val = y[i]
+    diff += (y_val - ((m * x_val) + b))
+  b_gradient = -(2/N) * diff  
+  return b_gradient
+
+def get_gradient_at_m(x, y, b, m):
+  N = len(x)
+  diff = 0
+  for i in range(N):
+      x_val = x[i]
+      y_val = y[i]
+      diff += x_val * (y_val - ((m * x_val) + b))
+  m_gradient = -(2/N) * diff  
+  return m_gradient
+
+#Your step_gradient function here
+def step_gradient(x, y, b_current, m_current):
+    b_gradient = get_gradient_at_b(x, y, b_current, m_current)
+    m_gradient = get_gradient_at_m(x, y, b_current, m_current)
+    b = b_current - (0.01 * b_gradient)
+    m = m_current - (0.01 * m_gradient)
+    return [b, m]
+
+months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+revenue = [52, 74, 79, 95, 115, 110, 129, 126, 147, 146, 156, 184]
+
+# current intercept guess:
+b = 0
+# current slope guess:
+m = 0
+
+b, m = step_gradient(months, revenue, b, m)
+print(b, m)
